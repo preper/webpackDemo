78123ad4153feb12b8ed01af49f86a0d4816731c
preper <qpreper@hotmail.com>
2018-07-30 17:12:18 +0800

    lazy loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-async function getComponent() {
-  var element = document.createElement('div')
-  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash')
+import _ from 'lodash'
 
+function component() {
+  var element = document.createElement('div')
+  var button = document.createElement('button')
+  var br = document.createElement('br')
+
+  button.innerHTML = 'Click me'
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.appendChild(br)
+  element.appendChild(button)
+
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default
+
+    print()
+  })
 
   return element
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component)
-})
+document.body.appendChild(component())
  
--- src/print.js
+++ src/print.js
@@ -1,3 +1,5 @@
+console.log('The print.js module has loaded.')
+
 export default function printMe () {
-  console.log('Updating print.js...')
+  console.log('Button Clicked')
 }
